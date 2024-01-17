98b5790f6cd0dc586f6be60b94b5794c86e9b2dd
harrisyoung27 <138505736+harrisyoung27@users.noreply.github.com>
2024-01-18 03:31:21 +0800

    Add files via upload

--- main_script.js
+++ main_script.js
@@ -1,3 +1,25 @@
 function goBack() {
     window.history.back();
   }
+
+  function checkCredentials() {
+            var email = document.getElementById("email").value;
+            var password = document.getElementById("password").value;
+            var secretDiv = document.getElementById("secretDiv");
+
+            // Check if the email and password are correct for either account
+            if ((email === "user@example.com" && password === "Admin1234") ||
+                (email === "anotheruser@example.com" && password === "AnotherPassword") ||
+                (email === "136799120242" && password === "idomypromises27")) {
+                alert("Authentication successful!");
+                location.href="homepage.html"
+            } else {
+                alert("Incorrect email or password. Try again.");
+            }
+        }
+
+        document.getElementById("password").addEventListener("keyup", function(event) {
+            if (event.keyCode === 13) {
+                checkCredentials();
+            }
+        });
